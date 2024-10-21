b5bfbdc124ab6877b065f8595c282107909bd8cf
Whiz <whiz@MacBook-Pro-de-Whiz.local>
2024-10-21 01:04:04 -0500

    base contract address

--- src/utils/contracts/contracts.js
+++ src/utils/contracts/contracts.js
@@ -1,4 +1,4 @@
 import ABI from './abi.json';
 
-export const CONTRACT_ADDRESS = '0x5Eeac913abB8327A4Cc56607C976Ae78F05E32a0';
+export const CONTRACT_ADDRESS = '0x2d5375E23dBcCaFB26b548f11a7d0b331C034b72';
 export const CONTRACT_ABI = ABI;
